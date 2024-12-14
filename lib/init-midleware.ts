eae2e164147c3d647ed0fbb270255dd99b4d2d6e
pkundu307 <pkundu307@gmail.com>
2024-12-14 11:06:05 +0530

    check1

--- lib/init-midleware.ts
+++ lib/init-midleware.ts
@@ -1,11 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+type MiddlewareFunction = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: (result?: unknown) => void
+) => void;
+
 // Initialize middleware
-const initMiddleware = (middleware: unknown) => {
+const initMiddleware = (middleware: MiddlewareFunction) => {
   return (req: NextApiRequest, res: NextApiResponse) =>
     new Promise((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      middleware(req, res, (result: any) => {
+      middleware(req, res, (result) => {
         if (result instanceof Error) {
           return reject(result);
         }
